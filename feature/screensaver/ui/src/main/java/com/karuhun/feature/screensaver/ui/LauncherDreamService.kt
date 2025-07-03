2c9017c39077fc8d6bd0739bd88972f835cae167
fajarxfce <nurul.10120014@mahasiswa.unikom.ac.id>
2025-07-03 12:01:38 +0700

    feat: implement screensaver functionality with Compose integration in LauncherDreamService

--- feature/screensaver/ui/src/main/java/com/karuhun/feature/screensaver/ui/LauncherDreamService.kt
+++ feature/screensaver/ui/src/main/java/com/karuhun/feature/screensaver/ui/LauncherDreamService.kt
@@ -17,12 +17,71 @@
 package com.karuhun.feature.screensaver.ui
 
 import android.service.dreams.DreamService
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.setViewTreeLifecycleOwner
+import androidx.savedstate.SavedStateRegistry
+import androidx.savedstate.SavedStateRegistryController
+import androidx.savedstate.SavedStateRegistryOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+import androidx.tv.material3.Text
 
-class LauncherDreamService : DreamService() {
+class LauncherDreamService : DreamService(), LifecycleOwner, SavedStateRegistryOwner {
+    private val lifecycleRegistry = LifecycleRegistry(this)
+    private val savedStateRegistryController = SavedStateRegistryController.create(this)
+
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
         isInteractive = false
         isFullscreen = true
-        setContentView(R.layout.feature_screensaver_ui_dream_layout)
+
+        savedStateRegistryController.performRestore(null)
+        lifecycleRegistry.currentState = Lifecycle.State.CREATED
+
+        val composeView = ComposeView(this).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setViewTreeLifecycleOwner(this@LauncherDreamService)
+            setViewTreeSavedStateRegistryOwner(this@LauncherDreamService)
+            setContent {
+                ScreenSaverScreen(
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+        setContentView(composeView)
+        lifecycleRegistry.currentState = Lifecycle.State.STARTED
+        lifecycleRegistry.currentState = Lifecycle.State.RESUMED
+    }
+
+    override val lifecycle: Lifecycle
+        get() = lifecycleRegistry
+
+    override val savedStateRegistry: SavedStateRegistry
+        get() = savedStateRegistryController.savedStateRegistry
+
+    override fun onDetachedFromWindow() {
+        lifecycleRegistry.currentState = Lifecycle.State.DESTROYED
+        super.onDetachedFromWindow()
+    }
+}
+
+@Composable
+fun ScreenSaverScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.background(Color.White)
+    ) {
+        Text(
+            text = "Screensaver",
+            modifier = modifier
+        )
     }
 }
